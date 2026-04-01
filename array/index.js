ff8291d10a0c8b92a20e2eed03ed076d48d64acd
AbdulMoiz34 <moiz05366@gmail.com>
2026-04-01 20:55:53 +0500

    array questions

---
+++ array/index.js
@@ -0,0 +1,119 @@
+// Array definition
+// Array is a linear data structure to store the multiple values in a Contiguous manner.
+// Linear means one after one-->> box-->box--->box
+// Contiguous means same address
+
+// Find the maximum number from an array.
+// const arr = [34, 23, 9, 12, 45, 98, 12];
+// let max = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//     if (max < arr[i]) max = arr[i];
+// }
+
+// console.log(max);
+
+// Find the second largest number from the array.
+// const arr = [34, 23, 9, 12, 45, 98, 12 , 61 , 98];
+
+// let largest = Math.max(arr[0], arr[1]);
+// let secondLargest = Math.min(arr[0], arr[1]);
+
+// for (let i = 2; i < arr.length; i++) {
+//     if (arr[i] > largest) {
+//         secondLargest = largest;
+//         largest = arr[i];
+//     } else if (arr[i] > secondLargest && arr[i] <largest) {
+//         secondLargest = arr[i];
+//     }
+// }
+
+// console.log("Largest No:" + largest);
+// console.log("Second largest No:" + secondLargest);
+
+// Reverse the array
+// const arr = [1, 2, 3, 4, 56, 62];
+
+// let i = 0;
+// let j = arr.length - 1;
+// while (i < j) {
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     i++, j--;
+// }
+
+// console.log(arr);
+
+// find the minimum number from an array
+// const arr = [1, 2, 3, 4, 56, 62, -4, -12];
+// let min = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         min = arr[i];
+//     }
+// }
+
+// console.log(min);
+
+// Find the second minium number from an array
+// const arr = [1, 2, 3, 4, 56, 62, -4, -12, -12];
+
+// let min = Math.min(arr[0], arr[1]);
+// let sMin = Math.max(arr[0], arr[1]);
+
+// for (let i = 2; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         sMin = min;
+//         min = arr[i];
+//     } else if (arr[i] < sMin && arr[i] !== min) sMin = arr[i];
+// }
+
+
+// console.log(`Second Minimum No: is ${sMin}`);
+
+
+// const arr = [0, 1, 1, 0, 0, 1, 1, 0, 1];
+// let j = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == 0) {
+//         let temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//         j++;
+//     }
+// }
+
+// console.log(arr);
+
+// negative numbers should be in the left side and positive should be in the right side.
+// const arr = [1, -2, -4, -9, 45, 68, 8274];
+// let i = 0;
+// let j = 0;
+
+// while (i < arr.length) {
+//     if (arr[i] < 0) {
+//         let temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//         j++;
+//     }
+//     i++;
+// }
+
+// console.log(arr);
+
+// rotate elements from left 1
+// const arr = [2, 3, 4, 5, 5];
+
+// let temp = arr[0];
+
+// for (let i = 0; i < arr.length - 1; i++) {
+//     arr[i] = arr[i + 1];
+// }
+
+// arr[arr.length - 1] = temp;
+
+// console.log(arr);
